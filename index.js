7c01777c9c7f44914aaf1c74c52f992dbef1e1d3
Natalie Umanzor <umanzor2949@gmail.com>
2017-12-13 04:39:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+// Note:     "node": "6.x", has been removed from package.json due to the following errors that prevented testing/passing
+// error java-script-logging-lab@0.1.0: The engine "node" is incompatible with this module. Expected version "6.x".
+//error Found incompatible module
+
+console.error("HALP!")
+console.log("I would be a logger.")
+console.warn("Warning")
